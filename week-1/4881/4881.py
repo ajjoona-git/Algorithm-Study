0e9eca0a4d8050aa44f3e20410ea5361c26e678f
Yang-Hyeon-Seo <ihyeonseo@naver.com>
2025-08-04 08:49:54 +0900

    [4881] 배열 최소 합

--- week-1/4881/4881.py
+++ week-1/4881/4881.py
@@ -1,77 +1,49 @@
 T = int(input())  # 테스트 케이스의 수
 
-def resize(arr, i, j):
-    '''주어진 배열(arr)에서 주어진 행(i)과 열(j)을 삭제한 배열을 반환한다.'''
-    if len(arr) <= 1:
-        return []
-    
-    # 원본 배열을 복사하여 진행한다.
-    resized_arr = arr[:]
-    # 행을 제거한다.
-    del resized_arr[i]
-    # 열을 제거하기 위해 행렬을 바꾼다.
-    resized_arr = list(map(list, zip(*resized_arr)))
-    # 행을 제거한다.
-    del resized_arr[j]
-	# 다시 원상복귀한다.
-    resized_arr = list(map(list, zip(*resized_arr)))
-
-    return resized_arr
-
-
-def find_minimum(arr):
-    '''주어진 2차원 배열(arr)의 최소값의 인덱스를 반환하는 함수
-    이때 각 행별 최대값과 최소값의 크기가 가장 큰 행의 최소값을 반환한다.
+def sumation(x, subtotal):
     '''
-    # n = len(arr)  # 배열의 크기
-    # # 최소 값을 담을 변수를 초기화한다.
-    # min_value = arr[0][0]
-    # x, y = 0, 0
-
-    # # 배열을 순회하면서
-    # for i in range(n):
-    #     for j in range(n):
-    #         # 현재 값이 최소 값보다 작다면
-    #         if arr[i][j] < min_value:
-    #             # 최소값을 갱신하고 인덱스를 저장한다.
-    #             min_value = arr[i][j]
-    #             x, y = i, j
+    주어진 행(x)에서 숫자를 하나 택하여 연산을 시작한다.
+    현재까지의 합(subtotal)이 최소 합(min_sum)보다 커지면 연산을 종료한다.
+    선택한 숫자의 행과 열의 visited를 True로 바꾸고, 현재까지의 합을 갱신한다.
+    '''
+    global min_sum
     
-    x = arr.index(max(arr, key=lambda x: max(x) - min(x)))
-    min_value = min(arr[x])
-    y = arr[x].index(min_value)
+    # 종료 조건1: 현재까지의 합(subtotal)이 최소 합(min_sum)보다 커지면 연산을 종료한다.
+    if subtotal >= min_sum:
+        return
+    
+    # 종료 조건2: 모든 행의 숫자를 택했다면 연산을 종료한다.
+    if x == N:
+        # min_sum을 갱신한다.
+        min_sum = subtotal
+        return
+        
+    # 해당 행의 모든 열을 순회하면서
+    for y in range(N):
+        # visited == False이면,
+        if not visited[y]:
+            # 숫자를 택하고 연산을 계속한다.
+            visited[y] = True
+            sumation(x+1, subtotal + numbers[x][y])
+            # 연산을 진행하다가 종료 조건으로 탈출하지 못한 경우
+            # 다른 숫자를 택하기 위해 visited를 False로 바꿔준다.
+            visited[y] = False
 
-    return min_value, x, y
-
-
+            
 for test_case in range(1, T+1):
     N = int(input())  # 배열의 크기
     # 배열을 입력받아 2차원 리스트로 저장한다.
     numbers = [ [int(i) for i in input().split()] for _ in range(N) ]
 
-    # 최소 숫자 합을 담을 리스트
-    sums = []
-
-
-    min_sum = 0
-    while numbers:
-        # # 종료 조건: 배열의 크기가 0이면 연산을 종료한다.
-        # if len(numbers) == 0:
-        #     break
-
-        # 최소 값을 찾는다.
-        min_num, x, y = find_minimum(numbers)
-        
-        # 숫자를 하나 택하면 숫자 합을 갱신하고,
-        min_sum += min_num
-        # 해당 숫자의 행,열을 삭제한 N-1xN-1 크기의 배열을 생성한다.
-        numbers = resize(numbers, x, y)
-
-        # 종료 조건: min_sum 값이 최소 합보다 커지면 연산을 종료한다.
-        # if min_sum > min(sums):
-        #     break
-    else:
-        # 연산을 끝까지 마쳤다면 sums 리스트에 값을 저장한다.
-        sums.append(min_sum)
+	# 선택한 숫자의 열을 선택하지 않기 위한 리스트
+    # 행은 중복해서 선택하지 않으므로 생략한다.
+    visited = [False] * N
+	
+    # 배열 최소 합을 초기화한다.
+    min_sum = float("inf")
     
-    print(f'#{test_case} {min(sums)}')
+    # 첫번째 행부터 연산을 시작한다.
+    sumation(0,0)
+    
+    # 결과 출력
+    print(f'#{test_case} {min_sum}')
