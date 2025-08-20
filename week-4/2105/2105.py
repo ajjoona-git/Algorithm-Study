508f0c27f15a2d5d30ffad9cd21527a6d12a586f
ajjoona <ajjoona@gmail.com>
2025-08-20 19:10:29 +0900

    [2105] 디저트 카페

---
+++ week-4/2105/2105.py
@@ -0,0 +1,88 @@
+# 2105. 디저트 카페
+
+
+def cafe_tour(r, c, width, height):
+    """(r, c)를 최상단으로 하는 카페 투어 경로를 탐색한다."""
+    global max_desserts
+    current_desserts = [cafe[r][c]]
+    start = (r, c)
+
+    # 카페 투어를 시작한다.
+    for i in range(4):
+        # 우하, 좌상의 경로 길이는 width
+        if i % 2 == 1:
+            # 직진하면서 유효한 디저트이면 탐색을 계속한다.
+            for k in range(1, width+1):
+                nr = r + dr[i]
+                nc = c + dc[i]
+                if (
+                    0 <= nr < N and 0 <= nc < N 
+                    and cafe[nr][nc] not in current_desserts
+                ):
+                    current_desserts.append(cafe[nr][nc])
+                else:
+                    return
+        
+        # 좌하, 우상의 경로 길이는 height
+        else:
+            for k in range(1, height+1):
+                nr = r + dr[i]
+                nc = c + dc[i]
+                if (
+                    0 <= nr < N and 0 <= nc < N 
+                    and cafe[nr][nc] not in current_desserts
+                ):
+                    current_desserts.append(cafe[nr][nc])
+                # 마지막에 출발지로 돌아오는 경우, 
+                # 투어를 성공적으로 마쳤으므로 최대값 갱신
+                elif (nr, nc) == start:
+                    if current_desserts > max_desserts:
+                        max_desserts = current_desserts
+                        return
+                else:
+                    return
+
+        # 방향 전환을 위해 현위치를 갱신한다.
+        r, c = nr - dr[i], nc - dr[i]
+
+
+T = int(input())
+for tc in range(1, T+1):
+    N = int(input())
+    cafe = [list(map(int, input().split())) for _ in range(N)]
+
+    # 카페 투어를 위한 델타 (시계방향, 우하-좌하-좌상-우상 순)
+    dr = [1, 1, -1, -1]
+    dc = [1, -1, -1, 1]
+
+    # 시작 위치
+    r, c = 0, 0
+    # 가장 많이 먹을 때의 디저트 수
+    max_desserts = -1
+
+    for r in range(1, N-1):
+        for c in range(N-1):
+            for width in range(1, N-1):
+                for height in range(1, N-1):
+                    cafe_tour(r, c, width, height)
+
+    print(f'{tc} {max_desserts}')
+            # current_desserts = [cafe[r][c]]
+
+            # # 카페 투어를 시작한다.
+            # for i in range(4):
+            #     nr = r + dr[i]
+            #     nc = c + dc[i]
+            #     # 벽에 부딪히거나 이미 먹은 디저트인 경우, 투어 종료
+            #     while(
+            #         not (0 <= nr < N and 0 <= nc < N) 
+            #         or cafe[nr][nc] in current_desserts
+            #     ):
+            #         # 디저트 리스트에 추가하고
+            #         current_desserts.append(cafe[nr][nc])
+            #         # 같은 방향으로의 탐색을 계속한다.
+            #         nr += dr[i]
+            #         nc += dc[i]
+                    
+            #     # 방향 전환을 위해 현위치를 갱신한다.
+            #     r, c = nr - dr[i], nc - dr[i]
---
+++ week-4/2105/README.md
@@ -0,0 +1,12 @@
+# 2105. 디저트 카페
+
+## 문제 출처
+[SWEA](https://swexpertacademy.com/main/talk/solvingClub/problemView.do?solveclubId=AZgvQCv6GNXHBIT9&contestProbId=AV5VwAr6APYDFAWu&probBoxId=AZjE2kOaAXDHBIO0&type=PROBLEM&problemBoxTitle=8%EC%9B%94+3-4%EC%A3%BC%EC%B0%A8%288%2F28%EA%B9%8C%EC%A7%80+%ED%91%B8%EC%8B%9C%EC%98%A4%29&problemBoxCnt=4)
+
+## 💡 접근 방식
+
+
+---
+
+## 💻 코드
+* [2105.py](2105.py)
