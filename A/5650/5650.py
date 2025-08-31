6477e6ddc3bac3e257d0baa64e120c2a80eb5ad8
ajjoona <ajjoona@gmail.com>
2025-08-31 19:11:10 +0900

    [5650] 핀볼 게임

--- A/5650/5650.py
+++ A/5650/5650.py
@@ -1,5 +1,9 @@
 # 5650. 핀볼 게임
 
+import sys
+sys.stdin = open("sample_input.txt")
+from pprint import pprint
+
 
 # 블록 번호별로 델타 인덱스 순서대로 블록에 접근했을 때 변하는 방향
 # 인덱스는 현재 진행 중인 방향, 값은 튕겨져 나가는 방향
@@ -9,6 +13,7 @@ blocks = {
     3: [2, 0, 3, 1],  # 좌, 상, 우, 하
     4: [1, 2, 3, 0],  # 하, 좌, 우, 상
     5: [1, 0, 3, 2],  # 하, 상, 우, 좌
+    'wall': [1, 0, 3, 2],  # 하, 상, 우, 좌
 }
 
 
@@ -16,12 +21,12 @@ blocks = {
 delta = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 
 
-def find_wormholes(arr, N):
+def find_wormholes(arr, n):
     """주어진 게임판에서 웜홀(6~10)의 위치를 저장해 딕셔너리 형태로 반환하는 함수"""
     # 웜홀의 위치를 찾아 저장한다.
     wormholes = {}
-    for r in range(N):
-        for c in range(N):
+    for r in range(n):
+        for c in range(n):
             if 6 <= arr[r][c] <= 10:
                 wormholes.setdefault(arr[r][c], [])
                 wormholes[arr[r][c]].append((r, c))
@@ -38,13 +43,64 @@ def find_wormholes(arr, N):
 def get_score(arr, N, r, c):
     """주어진 게임판에서 (r,c)를 시작점으로 할 때의 최대 점수를 반환하는 함수"""
     max_score = 0
+
     # 상하좌우 네 방향 탐색
     for i in range(4):
         score = 0
-        # 출발 위치로 돌아오거나 블랙홀(-1)에 빠지기 전까지 반복
-            nr, nc = r + delta[i][0], c + delta[i][1]
+        nr, nc = r + delta[i][0], c + delta[i][1]
+
+        while True:
+            # 벽이면 방향 전환, score +1
+            if not (0 <= nr < N and 0 <= nc < N):
+                nr -= delta[i][0]
+                nc -= delta[i][1]
+                i = blocks.get('wall')[i]
+                score +1
+                continue
+
+            # 종료 조건
+            # 출발 위치로 돌아오거나 블랙홀(-1)에 빠지기 전까지 반복
+            if (nr, nc) == (r, c) or arr[nr][nc] == -1:
+                # 최대 점수 갱신
+                max_score = max(max_score, score)   
+                break
+
             # 0이면 통과
-            # 1~5이면 blocks 방향 전환
+            # elif arr[nr][nc] == 0:
+            #     pass
+            
+            # 1~5이면 blocks 방향 전환, score +1
+            elif 1 <= arr[nr][nc] <= 5:
+                i = blocks.get(arr[nr][nc])[i]
+                score += 1
+
             # 6~10이면 wormhole_pair 위치 변경
+            elif 6 <= arr[nr][nc] <= 10:
+                nr, nc = wormhole_pair.get((nr, nc))
+                continue
+
+            nr, nc = nr + delta[i][0], nc + delta[i][1]
+        
 
     return max_score
+
+
+T = int(input())
+for tc in range(1, 1+1):
+    N = int(input())
+    grid = [list(map(int, input().split())) for _ in range(N)]
+
+    # 웜홀 찾기
+    wormhole_pair = find_wormholes(grid, N)
+
+    max_score = 0
+
+    # 게임판에서 0인 모든 공간에서 점수 계산
+    for r in range(N):
+        for c in range(N):
+            if grid[r][c] == 0:
+                local_max_score = get_score(grid, N, r, c)
+                # print(local_max_score)
+                max_score = max(max_score, local_max_score)
+    
+    print(f'#{tc} {max_score}')
