650801a1056619f67b7b8597ab952e83f7c66d52
ajjoona <ajjoona@gmail.com>
2025-09-01 23:04:58 +0900

    [5650] 핀볼 게임

--- A/5650/5650.py
+++ A/5650/5650.py
@@ -13,7 +13,6 @@ blocks = {
     3: [2, 0, 3, 1],  # 좌, 상, 우, 하
     4: [1, 2, 3, 0],  # 하, 좌, 우, 상
     5: [1, 0, 3, 2],  # 하, 상, 우, 좌
-    'wall': [1, 0, 3, 2],  # 하, 상, 우, 좌
 }
 
 
@@ -40,7 +39,7 @@ def find_wormholes(arr, n):
     return wormhole_pair
 
 
-def get_score(arr, N, r, c):
+def get_score(arr, r, c):
     """주어진 게임판에서 (r,c)를 시작점으로 할 때의 최대 점수를 반환하는 함수"""
     max_score = 0
 
@@ -51,12 +50,12 @@ def get_score(arr, N, r, c):
 
         while True:
             # 벽이면 방향 전환, score +1
-            if not (0 <= nr < N and 0 <= nc < N):
-                nr -= delta[i][0]
-                nc -= delta[i][1]
-                i = blocks.get('wall')[i]
-                score +1
-                continue
+            # if not (0 <= nr < N and 0 <= nc < N):
+            #     nr -= delta[i][0]
+            #     nc -= delta[i][1]
+            #     i = blocks.get('wall')[i]
+            #     score +1
+            #     continue
 
             # 종료 조건
             # 출발 위치로 돌아오거나 블랙홀(-1)에 빠지기 전까지 반복
@@ -69,10 +68,17 @@ def get_score(arr, N, r, c):
             # elif arr[nr][nc] == 0:
             #     pass
             
-            # 1~5이면 blocks 방향 전환, score +1
-            elif 1 <= arr[nr][nc] <= 5:
+            # 1~4이면 blocks 방향 전환, score +1
+            elif 1 <= arr[nr][nc] <= 4:
                 i = blocks.get(arr[nr][nc])[i]
                 score += 1
+
+            # 5(혹은 벽)이면 지금까지의 점수 *2하고 출발위치로 돌아감
+            # 현재까지의 루트를 되돌아갈 것이기 때문이다.
+            elif arr[nr][nc] == 5:
+                score = score * 2 + 1
+                nr, nc == r, c
+                continue
 
             # 6~10이면 wormhole_pair 위치 변경
             elif 6 <= arr[nr][nc] <= 10:
@@ -88,19 +94,22 @@ def get_score(arr, N, r, c):
 T = int(input())
 for tc in range(1, 1+1):
     N = int(input())
-    grid = [list(map(int, input().split())) for _ in range(N)]
+    # 벽과 5번 블록의 역할이 같으므로, grid의 외곽을 5로 패딩해준다.
+    grid = [[5] + list(map(int, input().split())) + [5] for _ in range(N)]
+    grid.insert(0, [5] * (N + 2))
+    grid.append([5] * (N + 2))
 
     # 웜홀 찾기
-    wormhole_pair = find_wormholes(grid, N)
+    wormhole_pair = find_wormholes(grid, N+2)
 
     max_score = 0
 
     # 게임판에서 0인 모든 공간에서 점수 계산
-    for r in range(N):
-        for c in range(N):
+    for r in range(1, N+1):
+        for c in range(1, N+1):
             if grid[r][c] == 0:
-                local_max_score = get_score(grid, N, r, c)
-                # print(local_max_score)
+                local_max_score = get_score(grid, r, c)
+                print(local_max_score)
                 max_score = max(max_score, local_max_score)
     
     print(f'#{tc} {max_score}')
