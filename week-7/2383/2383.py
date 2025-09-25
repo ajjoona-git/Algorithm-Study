e16a6ddeaae2be45bc99f9bc8d64e733ae1c4d01
ajjoona <ajjoona@gmail.com>
2025-09-26 00:42:05 +0900

    [2383] 점심 식사시간

--- week-7/2383/2383.py
+++ week-7/2383/2383.py
@@ -4,8 +4,7 @@
 각 사람에게 가장 가까운 계단을 찾는다 -> 배정한다?
     - 문제 예시에서 사람 5는 1번계단이 가까운데 2번 계단으로 감.
 
-계단은 2개로 고정.
-계단을 내려가고 있는 사람은 선입선출 -> 큐
+계단은 2개로 고정. 계단을 내려가고 있는 사람은 선입선출 -> 큐
 큐의 길이는 3, (계단 내려갈 때까지 남은 시간, 사람번호)를 저장하자.
     계단 내려갈 때까지 남은 시간이 0이면 dequeue
     아니면 다시 enqueue
@@ -24,6 +23,9 @@
     도착한 사람 리스트의 길이가 사람 수라면 종료
 계단에 있는 사람은 따로 큐로 관리한다.
 """
+import heapq
+import sys
+sys.stdin = open("input.txt")
 
 
 def get_initial_position():
@@ -51,32 +53,107 @@ def calculate_distance():
     for idx, (px, py) in enumerate(people):
         d1 = abs(px - sx1) + abs(py - sy1)
         d2 = abs(px - sx2) + abs(py - sy2)
-        distances[idx].extend([d1, d2])
+        distances[idx] = [d1, d2]
 
     return distances
 
 
-def update_stair(stair):
-    """계단 상황을 업데이트한다."""
-    new_stair = []
+def generate_combines(idx, picked_A, picked_B):
+    """각 사람이 계단 1을 이용하는 경우, 계단 2를 이용하는 경우"""
+    global min_time
 
-    while stair:
-        left_time, idx_person = stair.pop()
-        new_left_time = left_time - 1
-        if new_left_time == 0:
-            continue
-        new_stair.append((new_left_time, idx_person))
+    if idx == M:
+        # 시간 계산 후 최소 소요 시간 갱신
+        time_A = calculate_time(picked_A, 0)
+        time_B = calculate_time(picked_B, 1)
+        current_time = max(time_A, time_B)
 
-    return new_stair
+        min_time = min(min_time, current_time)
+        return
+    
+    # 첫 번째 계단 선택
+    generate_combines(idx + 1, picked_A + [idx], picked_B)
+    # 두 번째 계단 선택
+    generate_combines(idx + 1, picked_A, picked_B + [idx])
+
+
+def calculate_time(picked, stair_idx):
+    """모든 사람이 해당 계단을 내려가는 데 걸리는 시간을 계산한다."""
+    if not picked:
+        return 0
+
+    stair_len = stairs[stair_idx][0]
+
+    # 도착 시간(계단까지의 거리)을 기준으로 오름차순 정렬
+    arrival_time = [distances[idx][stair_idx] for idx in picked]
+    arrival_time.sort()
+
+    # 계단을 탈출한 시각을 저장할 우선순위큐
+    finish_times = []  
+    for arrival in arrival_time:
+        enter_stair_time = arrival + 1  # 계단을 내려가기 시작한 시각
+        
+        # 계단을 탈출한 사람이 3명 미만이라면
+        if len(finish_times) < 3:
+            start = enter_stair_time      
+        
+        # 계단이 꽉 차있는 상태
+        else:
+            # 가장 빨리 계단을 탈출하는 시각과 계단까지의 거리를 비교
+            earliest_finish = heapq.heappop(finish_times)
+            start = max(earliest_finish, enter_stair_time)
+
+        finish = start + stair_len
+        heapq.heappush(finish_times, finish)  
+
+    return max(finish_times)
+
+
+# def update_stair(stair):
+#     """계단 상황을 업데이트한다."""
+#     new_stair = []
+
+#     while stair:
+#         left_time, idx_person = stair.pop()
+#         new_left_time = left_time - 1
+#         if new_left_time == 0:
+#             continue
+#         new_stair.append((new_left_time, idx_person))
+
+#     return new_stair
+
+
+# def bfs():
+#     hq = []
+#     # (거리, 거리차(계단1-계단2), 사람, 계단)
+#     for idx, [dist1, dist2] in enumerate(distances):
+#         heapq.heappush(hq, (dist1, dist2 - dist1, idx, 0))
+#         heapq.heappush(hq, (dist2, dist1 - dist2, idx, 1))
+        
+#     while hq:
+#         d, diff_d, man_idx, stair_idx = heapq.heappop(hq)
+
+#         if arrived[man_idx]:
+#             continue
+
+#         if len(stairs_now[stair_idx]) >= 3:
+#             continue
+
+#         arrived[man_idx] = d
+#         stairs_now[stair_idx].append((stairs[stair_idx][0], man_idx))
+        
 
 
 T = int(input())
 for tc in range(1, T+1):
     N = int(input())
     grid = [list(map(int, input().split())) for _ in range(N)]
+
     people, stairs = get_initial_position()
     M = len(people)  # 사람 수
+    distances = calculate_distance()  # 계단까지의 거리 정보 [계단1, 계단2]
+    
+    min_time = float('inf')
+    generate_combines(0, [], [])
 
-    arrived = []  # 도착한 사람들을 저장
-
-
+    print(f'#{tc} {min_time}')
